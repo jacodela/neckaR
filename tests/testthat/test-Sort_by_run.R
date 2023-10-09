3a948a29dd261c2cf33d08a5ce0ef1b8847f6b0e
Jacobo <jacodela@gmail.com>
2023-10-09 14:54:39 +0200

    Create Sort_by_run test

---
+++ tests/testthat/test-Sort_by_run.R
@@ -0,0 +1,11 @@
+test_that("File vector is sorted", {
+	unsorted_vector = c("11_RUN01_LM16-133_NT5054.xlsx", "22_run10_LM16-135_NT5054.xlsx", "33_RuN02_LM16-134_NT5054.xlsx")
+	sorted_vector = c("11_RUN01_LM16-133_NT5054.xlsx", "33_RuN02_LM16-134_NT5054.xlsx",  "22_run10_LM16-135_NT5054.xlsx")
+	expect_equal(Sort_by_run(unsorted_vector), sorted_vector)
+})
+
+test_that("Output is vector", {
+	unsorted_vector = c("11_RUN01_LM16-133_NT5054.xlsx", "22_run10_LM16-135_NT5054.xlsx", "33_RuN02_LM16-134_NT5054.xlsx")
+	sorted_vector = c("11_RUN01_LM16-133_NT5054.xlsx", "33_RuN02_LM16-134_NT5054.xlsx",  "22_run10_LM16-135_NT5054.xlsx")
+	expect_vector(Sort_by_run(unsorted_vector), ptype = character(), size = length(unsorted_vector))
+})
